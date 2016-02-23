b46ba1fa503253787e36e1ff0aecbba97da60969
CarolineH <carolineherman@hotmail.fr>
2016-02-23 11:29:53 +0100

    create scaffold list and task and migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216095255) do
+ActiveRecord::Schema.define(version: 20160223101647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.integer  "state"
+    t.datetime "due_date"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "lists", ["user_id"], name: "index_lists_on_user_id", using: :btree
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name"
+    t.text     "content"
+    t.integer  "state"
+    t.integer  "list_id"
+    t.time     "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tasks", ["list_id"], name: "index_tasks_on_list_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +58,6 @@ ActiveRecord::Schema.define(version: 20160216095255) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "lists", "users"
+  add_foreign_key "tasks", "lists"
 end
